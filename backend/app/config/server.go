210621daecca7dde51a7b760cfa3d3dd362ebc16
Willie Bam <meilinli1013@gmail.com>
2026-08-05 09:29:36 +0800

    chore: store direct fields in config.go

--- backend/app/config/server.go
+++ backend/app/config/server.go
@@ -1,21 +1,12 @@
 package config
 
 import (
-	"time"
-
 	"github.com/WillieBam/support_copilot/backend/utils/server"
 )
 
 type IServer = server.IServer
 
 func NewServerConfig(name string) *server.ServerConfig {
-	return server.NewServerConfig(server.ServerConfigOptions{
-		Name: name,
-		PortGetter: func() int {
-			return Get().Http.Port
-		},
-		ShutDownTimeOutGetter: func() time.Duration {
-			return Get().Http.ShutdownTimeOut
-		},
-	})
+	c := Get()
+	return server.NewServerConfig(name, c.Http.Port, c.Http.ShutdownTimeOut)
 }
